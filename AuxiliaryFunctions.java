576c53a6d1b6c99e118710755445d4ee6a0a3766
Rodrigo Lucas Rosales <53952323+King1nDaNorth@users.noreply.github.com>
2024-11-12 20:14:35 -0300

    Create AuxiliaryFunctions.java

---
+++ AuxiliaryFunctions.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuxiliaryFunctions {
+
+    public static String[][] readCSV(String filePath) {
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
