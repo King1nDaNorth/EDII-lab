e02f0dc808265fe0d699a22090d2983ff10205d8
Rodrigo Lucas Rosales <53952323+King1nDaNorth@users.noreply.github.com>
2024-11-12 19:38:37 -0300

    Update Main.java
    Created main sections. What we need to to and where

--- Main.java
+++ Main.java
@@ -2,6 +2,14 @@ public class Main
 {
   public static void main (String args[] )
   {
+    // Extract data from file array
+    fetchData();
+    
+    // Transfer data from array to AVL and BST trees, virtually duplicating the data for future comparison
+    inputDataBST();
+    inputDataAVL();
+
+    // Process data
     
   }
 }
