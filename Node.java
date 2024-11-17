bb98757c38d951e6810e991f06d474a97439b5a4
Rodrigo Lucas Rosales <53952323+King1nDaNorth@users.noreply.github.com>
2024-11-17 02:33:55 -0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,11 +1,11 @@
 public class Node {
-    DadosEstacao estacao;    
+    DadosEstacao dados;    
     int height;
     Node left;
     Node right;
 
-    public Node(DadosEstacao estacao) {
-        this.estacao = estacao;   
+    public Node(DadosEstacao dados) {
+        this.dados = dados;   
         this.height = 1;
         left = null;
         right = null;
