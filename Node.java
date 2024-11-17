25b1117830fda591edfc2f6586207b78a17d6891
Rodrigo Lucas Rosales <53952323+King1nDaNorth@users.noreply.github.com>
2024-11-17 02:20:26 -0300

    Update Node.java
    Introduced Estacao struct for practicity

--- Node.java
+++ Node.java
@@ -1,32 +1,11 @@
 public class Node {
-    String estacao;
-    String linha;
-    
-    int ano;
-    int jan, fev, mar, abr, mai, jun, jul, ago, set, out, nov, dez;
-    
+    Estacao estacao;    
     int height;
     Node left;
     Node right;
 
-    public Node(String estacao, String linha, int ano, int jan, int fev, int mar, int abr, int mai, int jun, int jul, int ago, int set, int out, int nov, int dez) {
-        
-        this.estacao = estacao;
-        this.linha = linha;
-        this.ano = ano;
-        this.jan = jan;
-        this.fev = fev;
-        this.mar = mar;
-        this.abr = abr;
-        this.mai = mai;
-        this.jun = jun;
-        this.jul = jul;
-        this.ago = ago;
-        this.set = set;
-        this.out = out;
-        this.nov = nov;
-        this.dez = dez;
-        
+    public Node(Estacao estacao) {
+        this.estacao = estacao;   
         this.height = 1;
         left = null;
         right = null;
